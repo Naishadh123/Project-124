3860d51692ec4bb609a3e0ecbf5874a902e0119e
Naishadh123 <84559186+Naishadh123@users.noreply.github.com>
2022-10-19 19:43:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ video=createCapture(VIDEO);
 video.size(550,500);
 
 canvas=createCanvas(550,550);
-canvas.position(560,100);
+canvas.position(560,50);
 
 poseNet=ml5.poseNet(video,modelLoaded);
 poseNet.on('pose',gotPoses);
